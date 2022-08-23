20bb32043006a0e73c1e34044789ab03ba079cbf
Sam Wylie <sam.wylie20@gmail.com>
2022-08-24 11:12:16 +1200

    Added Plant Library data

--- src/components/plantLibrary.component.jsx
+++ src/components/plantLibrary.component.jsx
@@ -1,7 +1,56 @@
+import { useState, useEffect } from "react";
+import "./plantLibrary.component.scss";
+
 const PlantLibrary = () => {
+  const [plant, setPlant] = useState([]);
+  // Get all plants data
+  const getPlants = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/plants");
+      const jsonData = await response.json();
+      setPlant(jsonData);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getPlants();
+  }, []);
+
   return (
-    <div>
-      <h4>This is the Plant Library</h4>
+    <div className="container">
+      <div className="grid-padding">
+        <h3 className="text-center">Welcome to your Plant Library</h3>
+      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Type</th>
+            <th scope="col">Growth Time</th>
+            <th scope="col">Season</th>
+            <th scope="col">Ideal Sow</th>
+            <th scope="col">Ideal Harvest</th>
+            <th scope="col">Climate</th>
+            <th scope="col">Need Cover</th>
+          </tr>
+        </thead>
+        <tbody>
+          {plant.map((plant) => (
+            <tr>
+              <th scope="row">{plant.plant_name}</th>
+              <td>{plant.plant_type}</td>
+              <td>{plant.growth_time}</td>
+              <td>{plant.season}</td>
+              <td>{plant.ideal_plant_time}</td>
+              <td>{plant.ideal_harvest_time}</td>
+              <td>{plant.ideal_climate}</td>
+              <td>{plant.need_cover.toString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
