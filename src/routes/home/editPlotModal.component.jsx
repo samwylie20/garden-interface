4e476904fb70ac4d375bc54895c7056777bf8e85
Sam Wylie <sam.wylie20@gmail.com>
2022-08-25 17:09:10 +1200

    Building Edit plot modal

---
+++ src/routes/home/editPlotModal.component.jsx
@@ -0,0 +1,102 @@
+import React, { Fragment, useState } from "react";
+import { ReactDOM } from "react";
+import "./home.component.scss";
+
+const EditPlotModal = ({ open, children, onClose }) => {
+  const [inputs, setInputs] = useState({
+    plot_name: "",
+    size: "",
+    covered: "",
+  });
+  if (!open) return null;
+
+  const { plot_name, size, covered } = inputs;
+  const onChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { plot_name, size, covered };
+      const response = await fetch(
+        `http://localhost:5000/plot/${children.plot_id}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        }
+      );
+      window.location = "/";
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  return ReactDOM.createPortal(
+    <>
+      <div style={OVERLAY_STYLES} />
+      <div style={MODAL_STYLES}>
+        {children}
+        <h2 className="text-center">Build Plot</h2>
+        <form onSubmit={onSubmitForm}>
+          <input
+            type="text"
+            name="plot_name"
+            placeholder="Plot Name"
+            className="form-control my-3"
+            value={plot_name}
+            onChange={(e) => onChange(e)}
+          />
+          <input
+            type="text"
+            name="size"
+            placeholder="size"
+            className="form-control my-3"
+            value={size}
+            onChange={(e) => onChange(e)}
+          />
+          <input
+            type="text"
+            name="covered"
+            placeholder="True"
+            className="form-control my-3"
+            value={covered}
+            onChange={(e) => onChange(e)}
+          />
+          <button className="btn btn-success btn-block " type="submit">
+            Submit
+          </button>
+        </form>
+        <button className="btn btn-danger btn-block" onClick={onClose}>
+          Close
+        </button>
+      </div>
+    </>,
+    document.getElementById("portal")
+  );
+};
+
+export default EditPlotModal;
+
+// TODO RESTYLE LATER
+const MODAL_STYLES = {
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  backgroundColor: "#FFF",
+  padding: "50px",
+  zIndex: 1000,
+};
+
+// TODO RESTYLE LATER
+const OVERLAY_STYLES = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: "rgba(0,0,0,.7)",
+  zIndex: 1000,
+};
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useState, useEffect } from "react";
 import Modal from "./addPlotModal.component";
+import EditPlotModal from "./editPlotModal.component";
 import "./home.component.scss";
 
 const Home = () => {
@@ -63,8 +64,8 @@ const Home = () => {
           </div>
         </div>
         <div className="row justify-content">
-          <div className="grid-padding">
-            {plots.map((plot) => (
+          {plots.map((plot) => (
+            <div className="grid-padding">
               <div className="col-sm-4 well px-md-3">
                 <h5 className="text-center header-text-style">
                   {plot.plot_name}
@@ -73,9 +74,51 @@ const Home = () => {
                   Size: {plot.size} | Covered: {plot.covered.toString()} | ID:
                   {plot.plot_id}
                 </p>
-                {plot.plotUnits.map((item) => (
-                  <p>{item.plant_type}</p>
-                ))}
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Name</th>
+                      <th scope="col">Type</th>
+                      <th scope="col">Harvest</th>
+                      <th scope="col">Edit/Remove</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {plot.plotUnits.map((item) => (
+                      <tr className="unit-table-data">
+                        <th scope="row">{item.plant_type}</th>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>
+                          <button className="btn btn-warning btn-edit">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="12"
+                              height="8"
+                              fill="currentColor"
+                              class="bi bi-pen"
+                              viewBox="0 0 16 16"
+                            >
+                              <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
+                            </svg>
+                          </button>
+                          <button className="btn btn-danger btn-remove">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="12"
+                              height="12"
+                              fill="currentColor"
+                              class="bi bi-trash3"
+                              viewBox="0 0 16 16"
+                            >
+                              <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                            </svg>
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
 
                 <div className="grid-padding-button">
                   <button className="btn btn-success btn-space">
@@ -98,8 +141,8 @@ const Home = () => {
                 </ul>
               ))} */}
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </Fragment>
