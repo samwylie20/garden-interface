3c4ae591590592af8b8e0a5699c129d5a1880b2d
Sam Wylie <sam.wylie20@gmail.com>
2022-09-08 11:05:27 +1200

    Nav bar and home title edit

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,10 +6,10 @@ const Navigation = () => {
     <Fragment>
       <div className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="title-container" to="/">
-          <h6 className="navbar-brand">Home</h6>
+          <h6 className="text-2xl font-bold pl-2">Home</h6>
         </Link>
         <Link to="/plantlibrary">
-          <h6 className="navbar-brand">Plant Library</h6>
+          <h6 className="text-2xl font-bold pl-4">Plant Library</h6>
         </Link>
       </div>
       <Outlet />
