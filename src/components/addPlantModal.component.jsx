5a222dc3b6e45e8e578514d61b4af4a5edcbcc96
james-wylie <51935661+james-wylie@users.noreply.github.com>
2022-09-17 16:13:20 +1200

    Add outside click close for add plant modal

--- src/components/addPlantModal.component.jsx
+++ src/components/addPlantModal.component.jsx
@@ -76,7 +76,7 @@ export default function Modal({ open, children, onClose }) {
 
   return ReactDOM.createPortal(
     <>
-      <div style={OVERLAY_STYLES} />
+      <div style={OVERLAY_STYLES} onClick={ () => onClose() }/>
       <div style={MODAL_STYLES}>
         {children}
         <h4 className="text-center">Add new plant...</h4>
